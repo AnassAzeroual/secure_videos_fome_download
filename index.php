afc75eae5077e16a6cf624c6643700f664dad6e2
anass <azeroual.anass2015@gmail.com>
2021-11-03 21:19:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>try to download my video</title>
+</head>
+<body>
+    <video src="./videos/Nature_Beautiful.mp4" controls></video>
+</body>
+</html>
